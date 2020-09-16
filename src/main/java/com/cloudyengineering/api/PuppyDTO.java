ae618bc0a0492d570e26ec1c28227fb559787a8c
Anthony Ikeda <anthony.ikeda@gmail.com>
2020-09-16 16:23:35 -0700

    Split.io version

---
+++ src/main/java/com/cloudyengineering/api/PuppyDTO.java
@@ -0,0 +1,40 @@
+package com.cloudyengineering.api;
+
+public class PuppyDTO {
+    private String id;
+    private String name;
+    private String breed;
+    private String gender;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
